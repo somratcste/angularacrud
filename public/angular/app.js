b9222d9c422c8b6662a74c9ac4019fdc16a2d91f
somratcste <somratcste@gmail.com>
2017-04-11 13:31:03 +0530

    fix api

--- public/angular/app.js
+++ public/angular/app.js
@@ -2,4 +2,4 @@
  * Created by somrat on 4/11/17.
  */
 var app = angular.module('getSupplier', [])
-    .constant('API_URL','http://localhost:8080/api/');
+    .constant('API_URL','http://angularacrud.cste/api/');
